603184792ead4e398eb6c5cc4aafced05c177a11
Alexis <rangel.angel.1cet22@gmail.com>
2026-04-13 21:37:30 -0900

    modified:   Css/contact.css 	modified:   Css/menu.css 	modified:   Css/style.css 	new file:   Img/Drinks/alc-1.jpg 	new file:   Img/Drinks/alc-2.jpg 	new file:   Img/Drinks/alc-3.jpg 	new file:   Img/Drinks/alc-4.jpg 	new file:   Img/Drinks/alc-5.jpg 	new file:   Img/Drinks/alc-6.jpg 	new file:   Img/Drinks/alc-7.jpg 	new file:   Img/Drinks/beer1.jpg 	new file:   Img/Drinks/beer2.jpg 	new file:   Img/Drinks/beer3.webp 	new file:   Img/Drinks/beer4.jpg 	new file:   Img/Drinks/prep-1.jpg 	new file:   Img/Drinks/prep-2.jpg 	new file:   Img/Drinks/prep-3.jpg 	new file:   Img/Drinks/soda-1.png 	new file:   Img/Drinks/soda-2.png 	new file:   Img/Drinks/soda-3.png 	new file:   Img/Drinks/soda-4.png 	new file:   Img/Drinks/soda-5.png 	new file:   Img/Drinks/soda-6.png 	new file:   Img/Drinks/soda-7.png 	new file:   Img/Drinks/vasos.png 	new file:   Img/Drinks/water-1.png 	new file:   Img/Drinks/water-2.png 	new file:   Img/Drinks/water-3.png 	new file:   Img/Drinks/water-4.png 	new file:   Img/Drinks/water-5.png 	new file:   Img/Drinks/water-6.webp 	new file:   Img/Drinks/water-7.png 	new file:   Img/Drinks/water-8.jpg 	new file:   Img/Jaiba_soporte.png 	new file:   Img/Kids/dessert-1.jpg 	new file:   Img/Kids/dessert-2.avif 	new file:   Img/Kids/dessert-3.jpg 	new file:   Img/Kids/kids-drink-1.jpg 	new file:   Img/Kids/kids-drink-2.jpeg 	new file:   Img/Kids/kids-drink-3.webp 	new file:   Img/Kids/kids-plate-1.jpg 	new file:   Img/Kids/kids-plate-2.jpg 	new file:   Img/Kids/kids-plate-3.jpg 	new file:   Img/Kids/kids-plate-4.jpg 	new file:   Img/Plates/entrada-1.jpg 	new file:   Img/Plates/entrada-2.jpg 	new file:   Img/Plates/entrada-3.jpg 	new file:   Img/Plates/entrada-4.jpg 	new file:   Img/Plates/entrada-5.jpg 	new file:   Img/Plates/entrada-6.jpg 	new file:   Img/Plates/entrada-7.jpg 	new file:   Img/Plates/entrada-8.jpg 	new file:   Img/Soups/soup-1.jpg 	new file:   Img/Soups/soup-2.webp 	new file:   Img/Soups/soup-3.jpg 	new file:   Img/Soups/soup-4.avif 	new file:   Img/Special/es-1.jpg 	new file:   Img/Special/es-2.jpg 	new file:   Img/Special/es-3.webp 	new file:   Img/Special/es-4.jpg 	new file:   Img/Special/es-5.jpg 	new file:   Img/Special/es-6.jpg 	new file:   Img/Special/es-7.jpg 	new file:   Img/Strong/strong-2.jpg 	new file:   Img/Strong/strong-3.jpg 	new file:   Img/Strong/strong-4.jpg     new file:   Img/Strong/strong-5.avif 	new file:   Img/Strong/strong-6.jpg 	new file:   Img/Strong/strong-7.jpg 	modified:   Js/script.js 	modified:   contact.html 	modified:   index.html 	modified:   menu.html

--- Js/script.js
+++ Js/script.js
@@ -12,6 +12,25 @@ function toggle_menu(){
 
 }
 
+function process_data(){
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let message = document.getElementById("message").value;
+    
+    var texto = "Nombre de usuario: "+name+"\n Correo: "+email+"\n Teléfono: "+phone+"\n"+message+"\n No tenemos base de datos, pero al menos el formulario funciona :v";
+    var enlace = document.createElement("a");
+    enlace.setAttribute("href", "data:text/plain; charset:utf-8,"+encodeURIComponent(texto));
+    enlace.setAttribute("download", "documento.txt");
+    enlace.style.display="none";
+    
+    document.body.appendChild(enlace);
+    enlace.click();
+
+    document.body.removeChild(enlace);
+}
+
+
 function show_details_1(){
     let detail = document.getElementById("info1");
     let photo2 = document.getElementById("photo2");
@@ -59,4 +78,51 @@ function show_details_3(){
         photo1.style.display = "none";
         photo2.style.display = "none";
     }
+}
+//Para el formulario de la seccion de contacto
+function send(){
+    document.getElementById("texto_para_form").innerHTML = "Gracias por sus comentarios, en breve nos contactaremos con usted";
+}
+
+// Elementos para el formulario de reservas
+
+var inputDate = document.getElementById("date-reserv");
+var inputHours = document.getElementById("hour-set");
+var selPersons = document.getElementById("sel-person");
+var buttonReserve = document.getElementById("btn_reservar");
+var dialog = document.getElementById("reservation-dialog");
+
+inputDate.addEventListener('input', (evento) =>{
+    const valor = evento.target.value;
+    
+    if (!esFechaFutura(valor)) {
+    alert("La fecha no puede ser anterior a hoy.");
+    inputDate.value = ''; // Limpiar el input
+   } 
+   else{
+     inputHours.disabled = false;
+     selPersons.disabled = false;
+     buttonReserve.disabled = false;
+   }
+});
+function esFechaFutura(fechaInput) {
+    const hoy = new Date();
+    hoy.setHours(0, 0, 0, 0); // Resetear horas para comparar solo días
+    const fechaSeleccionada = new Date(fechaInput);
+    return fechaSeleccionada >= hoy;
+}
+function reserve(){
+    dialog.style.display = "flex";
+    dialog.style.opacity = "1";
+    let date = document.getElementById("date-reserv").value;
+    let time = document.getElementById("hour-set").value;
+    let persons = document.getElementById("sel-person").value;
+
+    document.getElementById("date-day").innerHTML = date.toString();
+    document.getElementById("date-hour").innerHTML = time.toString();
+    document.getElementById("date-members").innerHTML = persons.toString();
+}
+function close_dialog(){
+    dialog.opacity = "0";
+    dialog.style.display = "none";
 }
